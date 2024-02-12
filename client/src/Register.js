4b200463a02fded8983c2baa2d89b3958d3444da
Roshan Chathuranga <147641397+Roshan-Coding@users.noreply.github.com>
2024-02-12 08:20:03 +0530

    sign up connected

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function Register() {
+
+    const [values, setValues] = useState({
+        name:'',
+        email:'',
+        password:''
+    })
+
+    const submit = async(e) => {
+        e.preventDefault();
+        try{
+            const result = await axios.post('http://localhost:1000/register',values);
+            console.log(result)
+            alert(result.data.Status);
+
+        }catch(err){
+            console.error(err);
+        }       
+    }
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='bg-white p-3 rounded w-25'>
             <h2>Sign-Up</h2>
-            <form>
+            <form onSubmit={submit}>
                 <div className='mb-3'>
                     <lable htmlFor='name'><strong>Name</strong></lable>
                     <input type='text' placeholder='Enter Name' name='name'
-                    className='form-control rounded-0'/>
+                    onChange={e => setValues({...values, name: e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'>
                     <lable htmlFor='email'><strong>Email</strong></lable>
                     <input type='email' placeholder='Enter Email' name='email'
-                    className='form-control rounded-0'/>
+                    onChange={e => setValues({...values, email: e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'>
                     <lable htmlFor='password'><strong>Password</strong></lable>
                     <input type='password' placeholder='Enter Password' name='password'
-                    className='form-control rounded-0'/>
+                    onChange={e => setValues({...values, password: e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <button type='submit' className='btn btn-success w-100 rounded-0'>Sign Up</button>
                 <p>You are agree to our terms and policies</p>
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cookie = require('cookie-parser');
+const salt = 10;
 
 const app = express();
 app.use(express.json());
@@ -15,9 +16,28 @@ const db = mysql.createConnection({
     user:"root",
     password:"",
     database:"signup"
+    //port:3306
 });
+    db.connect();
 
-
+app.post('/register',(req, res)=>{
+    const data = req.body;
+    //console.log(data)
+    const sql = "INSERT INTO login (`name`, `email`, `password`) VALUES(?)";
+    bcrypt.hash(data.password.toString(), salt, (err, hash) => {
+        if(err) return res.json({Error:'error in hashing password'})
+        const values = [
+            data.name,
+            data.email,
+            hash
+        ]
+       // console.log(db)
+        db.query(sql, [values], (err, result) => {
+            if(err) return res.json({Error: 'Inserting data error in server'});
+            return res.json({Status:'Registered Successfully'});
+        })
+    })    
+})
 
 app.listen(1000,()=>{
     console.log("app is connected");
